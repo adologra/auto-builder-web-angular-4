4be83bacb6f703998ff3c59dfee27e370dee5599
Adolfo Lopez Granados <adologra@gmail.com>
2017-06-24 15:49:23 +0200

    Added CSS to H1, Added a dinamic table of content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
-import { MarkdownComponent } from 'angular2-markdown';
+import { MarkdownComponent, MarkdownService } from 'angular2-markdown';
 
 import { AppComponent }  from './components/app.component';
 import { HeaderComponent }  from './components/panels/header/header.component';
 import { ContentComponent }  from './components/panels/content/content.component';
+import { LeftMenuComponent } from './components/panels/content/left.menu.component';
 import { FooterComponent }  from './components/panels/footer/footer.component';
 import { StructureConfigurationService } from './services/configuration/structureConfigurationService';
-import { MarkdownService } from 'angular2-markdown';
+
 
 @NgModule({
   imports:      [ 
@@ -21,6 +22,7 @@ import { MarkdownService } from 'angular2-markdown';
                     HeaderComponent,
                     MarkdownComponent,
                     ContentComponent,
+                    LeftMenuComponent,
                     FooterComponent
                     
   				],
--- src/app/components/panels/content/content.component.ts
+++ src/app/components/panels/content/content.component.ts
@@ -1,16 +1,51 @@
 import { Component } from '@angular/core';
 import { MarkdownService } from 'angular2-markdown';
+import { ItemMenu } from '../../../models/types/item.menu';
+import { LeftMenuComponent } from './left.menu.component';
 
 @Component({
-  selector: 'main[class="main-content-wrapper"]',
+  selector: 'section[class="main-content-wrapper"]',
+  entryComponents: [ 
+                LeftMenuComponent
+            ],
   templateUrl: 'app/views/components/panels/content/contentTemplate.html'
 })
 export class ContentComponent  { 
+    leftMenuItems: ItemMenu[];
     constructor(private _markdown: MarkdownService) {
         console.log("markdown created");
+        this.leftMenuItems = new Array<ItemMenu> ();
     }
 
 	ngOnInit () {
+        var that = this;
+        this._markdown.renderer.heading = function (text: String, level: Number) {
+            var escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
+            var className = '';
+            that.addMenuItems(escapedText, text, level);
+            if (level === 1) {
+                className = 'main-content-title';
+            }            
+            return '<h' + level + ' id="' + escapedText + '" class="' + className + '">' + 
+                text + '</h' + level + '>';
+        };
+        
         
 	}
+    
+    private addMenuItems(escapedText: String, text: String, level: Number) {
+        var title:ItemMenu = new ItemMenu();
+        title.setId('#'+escapedText);
+        title.setLevel(level);
+        title.setText(text);
+        this.leftMenuItems.push(title);
+    }
+    
+    public showLeftMenu (): Boolean {
+        return this.leftMenuItems && this.leftMenuItems.length > 0;
+    }
+    
+    public showRightMenu (): Boolean {
+        return false;
+    }
 }
