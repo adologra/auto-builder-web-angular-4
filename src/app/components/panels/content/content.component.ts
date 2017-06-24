4b961756dafae5e5041afadd3bd614d128407939
Adolfo Lopez Granados <adologra@gmail.com>
2017-06-24 15:49:23 +0200

    Added CSS only to H1

--- src/app/components/panels/content/content.component.ts
+++ src/app/components/panels/content/content.component.ts
@@ -8,9 +8,21 @@ import { MarkdownService } from 'angular2-markdown';
 export class ContentComponent  { 
     constructor(private _markdown: MarkdownService) {
         console.log("markdown created");
+        this.leftMenu = [];
     }
 
 	ngOnInit () {
-        
+        var that = this;
+        this._markdown.renderer.heading = function (text, level) {
+            var escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
+            var className = '';
+            if (level === 1) {
+                var title = {id:'#'+escapedText};
+                that.leftMenu.push(title);
+                className = 'main-content-title';
+            }            
+            return '<h' + level + ' id="' + escapedText + '" class="' + className + '">' + 
+                text + '</h' + level + '>';
+        };
 	}
 }
